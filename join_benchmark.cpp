f54545d9551aef3280389d7de670210c26710685
Richard Ebeling <He3lixxx@users.noreply.github.com>
2023-05-19 16:36:13 +0200

    Generate data in main

--- join_benchmark.cpp
+++ join_benchmark.cpp
@@ -36,36 +36,35 @@ size_t count_tuples_with_join_partners(std::vector<std::string> left_side, std::
 
 constexpr std::string_view ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
+static std::vector<std::string> left_tuples(LEFT_SIDE_TUPLES);
+static std::vector<std::string> right_tuples(RIGHT_SIDE_TUPLES);
+
 template <typename T>
 uint64_t int_between_0_and(T&& rng, uint64_t max) {
   return rng() % max;
 }
 
-void BM_Join(benchmark::State& state) {
-  static std::vector<std::string> left_tuples(LEFT_SIDE_TUPLES);
-  static std::vector<std::string> right_tuples(RIGHT_SIDE_TUPLES);
+void generate_data() {
+  std::mt19937_64 rng{std::random_device{}()};
 
-  // Only generate test data on first run to reduce influence on profiling results
-  if (left_tuples[0].size() == 0) {
-    std::mt19937_64 rng{std::random_device{}()};
-
-    for (auto& value : left_tuples) {
-      value.resize(STRING_LENGTH);
-      std::generate(value.begin(), value.end(), [&]() { return ALPHABET[int_between_0_and(rng, ALPHABET.size())]; });
-    }
-
-    std::bernoulli_distribution join_partner_distribution(JOIN_PARTNER_PROBABILITY);
-
-    for (auto& value : right_tuples) {
-      value.resize(STRING_LENGTH);
-      if (join_partner_distribution(rng)) {
-        value = left_tuples[int_between_0_and(rng, left_tuples.size())];
-      } else {
-        std::generate(value.begin(), value.end(), [&]() { return ALPHABET[int_between_0_and(rng, ALPHABET.size())]; });
-      }
-    }
+  for (auto& value : left_tuples) {
+    value.resize(STRING_LENGTH);
+    std::generate(value.begin(), value.end(), [&]() { return ALPHABET[int_between_0_and(rng, ALPHABET.size())]; });
   }
 
+  std::bernoulli_distribution join_partner_distribution(JOIN_PARTNER_PROBABILITY);
+
+  for (auto& value : right_tuples) {
+    value.resize(STRING_LENGTH);
+    if (join_partner_distribution(rng)) {
+      value = left_tuples[int_between_0_and(rng, left_tuples.size())];
+    } else {
+      std::generate(value.begin(), value.end(), [&]() { return ALPHABET[int_between_0_and(rng, ALPHABET.size())]; });
+    }
+  }
+}
+
+void BM_Join(benchmark::State& state) {
   for (auto _ : state) {
     auto result = count_tuples_with_join_partners(left_tuples, right_tuples);
     benchmark::DoNotOptimize(result);
@@ -75,6 +74,19 @@ void BM_Join(benchmark::State& state) {
       benchmark::Counter(static_cast<double>(state.iterations() * (left_tuples.size() + right_tuples.size())),
                          benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
 }
-BENCHMARK(BM_Join)->Iterations(20);
+BENCHMARK(BM_Join)->Iterations(10);
 
-BENCHMARK_MAIN();
+int main(int argc, char** argv) {
+  generate_data();
+
+  char arg0_default[] = "benchmark";
+  char* args_default = arg0_default;
+  if (!argv) {
+    argc = 1;
+    argv = &args_default;
+  }
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  ::benchmark::RunSpecifiedBenchmarks();
+  ::benchmark::Shutdown();
+}
